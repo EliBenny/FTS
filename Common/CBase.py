c230f22a84425c54a1bea716e972fbf97547adf6
Eli Benny <elibenny@gmail.com>
2017-04-02 12:47:15 +0300

    progress base class

--- Common/CBase.py
+++ Common/CBase.py
@@ -9,6 +9,7 @@ class CBase:
         fileNameConfig = join('Common', 'configGlobal.json')
         with open(fileNameConfig) as data_file:
             configGlobal = json.load(data_file)
+
         # address config file
         className = self.__class__.__name__[1:]
         configPath = \
@@ -18,9 +19,25 @@ class CBase:
                     configGlobal['config'][0]['initiFileName'] + className +
                     "." + configGlobal['config'][0]['format']
                 )
-
+        # load config file
         fileNameConfig = join(configPath)
         with open(fileNameConfig) as data_file:
             self.config = json.load(data_file)
 
+        # address doc path
+        self.docPath = \
+            join \
+                (
+                   className, configGlobal['doc'][0]['subfolder'],
+                   configGlobal['doc'][0]['initiFileName'] + className +
+                   "." + configGlobal['doc'][0]['format']
+                )
 
+        # address test path
+        self.testPath = \
+            join \
+                    (
+                    className, configGlobal['test'][0]['subfolder'],
+                    configGlobal['test'][0]['initiFileName'] + className +
+                    "." + configGlobal['test'][0]['format']
+                )
