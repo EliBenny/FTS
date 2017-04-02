b248e70a99d31f7fc28eb555730439de1d596131
Eli Benny <elibenny@gmail.com>
2017-04-02 11:52:32 +0300

    base class is done to read config file

--- Common/CBase.py
+++ Common/CBase.py
@@ -1,4 +1,26 @@
-class CBase(object):
+import json
+from os.path import join
+
+
+class CBase:
 
     def __init__(self):
-        self.configPath = self.__class__.__name__
+        # read json
+        fileNameConfig = join('Common', 'configGlobal.json')
+        with open(fileNameConfig) as data_file:
+            configGlobal = json.load(data_file)
+        # address config file
+        className = self.__class__.__name__[1:]
+        configPath = \
+            join\
+                (
+                    className, configGlobal['config'][0]['subfolder'],
+                    configGlobal['config'][0]['initiFileName'] + className +
+                    "." + configGlobal['config'][0]['format']
+                )
+
+        fileNameConfig = join(configPath)
+        with open(fileNameConfig) as data_file:
+            self.config = json.load(data_file)
+
+
