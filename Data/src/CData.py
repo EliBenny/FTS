dde6c26079b6893978811b17eee2ac351ac9981a
Eli Benny <elibenny@gmail.com>
2017-04-02 23:32:52 +0300

    distributing training set and test set

--- Data/src/CData.py
+++ Data/src/CData.py
@@ -1,7 +1,7 @@
 from Common.CBase import CBase
 from os.path import join
-import csv
 from pandas import read_csv
+import numpy as np
 
 class CData(CBase):
 
@@ -16,10 +16,25 @@ class CData(CBase):
 
 
     def ReadData(self, pathPic, pathClass, config):
-        dataPic = read_csv(pathPic, sep=',', header=None, skiprows = 1)
-        dataClass = read_csv(pathClass, sep=',', header=None, skiprows=1)
+        dataPic = read_csv(pathPic, sep = ',', header = None, skiprows = 1)
+        dataClass = read_csv(pathClass, sep = ',', header = None, skiprows = 1)
+        classification = dataClass.get_values()
         # manage training-set test-set
-        self.pic = dataPic.get_values()
-        self.classification = dataPic.get_values()
+        incTest = int(1.0 / float(config['ratioTest']))
+
+        pic = dataPic.get_values()
+        pic0 = pic[classification == 0]
+
+        picTest0 = pic0[np.arange(0, pic0.size, incTest)]
+        picTrain0 = pic0[~np.arange(0, pic0.size, incTest)]
+
+
+        pic1 = pic[classification == 1]
+
+        picTest1 = pic1[np.arange(0, pic1.size, incTest)]
+        picTrain1 = pic1[~np.arange(0, pic1.size, incTest)]
+        self.trainingSet.pic1 = np.reshape(picTrain1, (48, 48), 'C')
+
+
         dummy=1;
 
